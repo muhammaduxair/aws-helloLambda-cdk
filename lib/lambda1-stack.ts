294d3b021554bc98c0c28c4377aa8074c72a7923
muhammaduxair <uxairjaaan@gmail.com>
2021-06-08 11:02:45 +0500

    aws s3 bucket with api gateway making rest api

--- lib/lambda1-stack.ts
+++ lib/lambda1-stack.ts
@@ -1,9 +1,27 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class Lambda1Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
+
+    const api = new apigw.LambdaRestApi(this, "myLambdaEndpoint", {
+      handler: hello,
+      proxy: false,
+    });
+
+    // api is accept only cars url
+    const items = api.root.addResource("/");
+    items.addMethod("GET"); // GET /items
+
+    const items2 = api.root.addResource("/hello");
+    items2.addMethod("GET"); // GET /items
   }
 }
